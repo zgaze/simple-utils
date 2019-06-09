4eadd5e92ece0fba6b7c2da2052365272f7d8612
zzy <942744575@qq.com>
2019-06-09 23:18:22 +0800

    inti

---
+++ time/time.cpp
@@ -0,0 +1,29 @@
+/* ***********************************************************************
+
+  > File Name: time.cpp
+  > Author: zzy
+  > Mail: 942744575@qq.com 
+  > Created Time: Sun 09 Jun 2019 11:18:11 PM CST
+
+ ********************************************************************** */
+
+#include <stdio.h>
+#include <vector>
+#include <string>
+#include <stdio.h>
+#include <climits>
+#include <gtest/gtest.h>
+
+using std::vector;
+using std::string;
+
+TEST(testCase,test0) {
+
+}
+
+
+int main(int argc, char* argv[]) {
+    testing::InitGoogleTest(&argc,argv);
+    return RUN_ALL_TESTS();
+}
+
